03a58cfc401aa8f92b9253305857cabdf877aadc
Ronald Rosete <ronaldtorresrosete30@gmail.com>
2026-05-03 12:22:19 +0800

    Update auto-commit-git.sh
    Add spaces for readability and clean code

--- auto-commit-git.sh
+++ auto-commit-git.sh
@@ -7,7 +7,7 @@ for ((i = 1; i <= $NUM_OF_ITERATIONS; i++)); do
     echo "######################################################"
     echo "#######          Creating File                 #######"
     echo "######################################################"
-    echo "Dummy-Text-for-Commit" >>dummy_file_$i.dat
+    echo "Dummy-Text-for-Commit" >> dummy_file_$i.dat
     git add ./
     git commit -m "Add dummy_file_$i.dat"
     git push -u origin main
