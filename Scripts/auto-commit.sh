9994da4be7188ef7106b3989b6d9c09d4084b176
engrron30 <ronaldtorresrosete30@gmail.com>
2026-05-03 13:12:28 +0800

    Add dummy_file_1.dat

--- Scripts/auto-commit.sh
+++ Scripts/auto-commit.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-NUM_OF_ITERATIONS=15
+NUM_OF_ITERATIONS=1
 
-for ((i = 1; i <= $NUM_OF_ITERATIONS; i++)); do
-
+create_file() {
     echo "######################################################"
     echo "#######          Creating File                 #######"
     echo "######################################################"
     echo "Dummy-Text-for-Commit" >> dummy_file_$i.dat
-    git add ./
-    git commit -m "Add dummy_file_$i.dat"
-    git push -u origin main
-    echo ""
+}
 
+delete_file() {
     echo "######################################################"
     echo "#######          Deleting File                 #######"
     echo "######################################################"
     rm dummy_file_$i.dat
+}
+
+commit_to_remote_repo() {
     git add ./
-    git commit -m "Remove dummy_file_$i.dat"
+    git commit -m "Add dummy_file_$i.dat"
     git push -u origin main
     echo ""
+}
+
+for ((i = 1; i <= $NUM_OF_ITERATIONS; i++)); do
+
+    create_file $i
+    commit_to_remote_repo $i
+
+    delete_file $i
+    commit_to_remote_repo $i
 
 done
